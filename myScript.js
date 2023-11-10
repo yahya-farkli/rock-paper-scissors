49d1190d4882ef624bebc429b5ec2b77529ec1cd
yahya-farkli <yahyafarkli63@gmail.com>
2023-11-10 12:02:32 +0100

    Finish the main scripts

--- myScript.js
+++ myScript.js
@@ -1,50 +1,51 @@
-const x = "rock";
-const y = "paper";
-const z = "scissors";
-let choiceArray = [x, y, z];
+let player = 0;
+let computer = 0;
+let choiceArray = ["Rock", "Paper", "Scissors"];
+
+
+let para = document.getElementById('para') ;
+
 
 function getComputerChoice(choiceArray) {
     return choiceArray[Math.floor(Math.random() * choiceArray.length)];
 }
 
-let input = prompt("Enter something").toLowerCase();
-
-const playerSelection = input;
-const computerSelection = getComputerChoice(choiceArray);
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection !== x && playerSelection !== y && playerSelection !== z) {
-        alert("Please enter a valid move");
-    
-   //used this fiirst but i thought it was too long then i found this new way of doing it seems neet and i like it
-    
-    /*if (!choiceArray.includes(playerSelection)) {
-        console.log("Please enter a valid move");*/
-    }else {
-        if (playerSelection == computerSelection) {
-            alert("draw")            
-        } else if (playerSelection == x && computerSelection == z){
-            alert("SCISSORS! , you won but i'm sure it's newbie luck")
-        } else if (playerSelection == y && computerSelection == x) {
-            alert("damn i underestimated you, you seem a lot more skilled than you look")
-        } else if (playerSelection == z && computerSelection == y) {
-            alert("I LOST! Impossible I'll DESTROY YOU NEXT")
-        } else if (playerSelection == z && computerSelection == x) {
-            alert("comeback another time newbie, maybe when you'are strong enough")
-        } else if(playerSelection == x && computerSelection == y) {
-            alert("You underestimated me, I'm a lot more skilled than I seem")
-        } else if(playerSelection == y && computerSelection == z) {
-            alert("YOU CAN NEVER DESTROY ME I'M INSTOPABLE!!! HAHAHHAHAHAH")
-            
-        } {
-            
-        }
-    }
-    
-  }
-  for (let round = 1; round <= 10; round++) {
-    let input = prompt("Enter something").toLowerCase();
-    const playerSelection = input;
+function playRound(playerSelection) {
     const computerSelection = getComputerChoice(choiceArray);
-    playRound(playerSelection, computerSelection);
+     
+            if (playerSelection == computerSelection) {
+                alert("draw")
+                computer++ && player++
+            }else if (playerSelection == "Rock" && computerSelection == "Scissors"){
+                alert("SCISSORS! , you won but i'm sure it's newbie luck") 
+                player++
+            } else if (playerSelection == "Paper" && computerSelection == "Rock") {
+                alert("damn i underestimated you, you seem a lot more skilled than you look")
+                player++ 
+            } else if (playerSelection == "Scissors" && computerSelection == "Paper") {
+                alert("I LOST! Impossible I'll DESTROY YOU NEXT")
+                player++ 
+            } else if (playerSelection == "Scissors" && computerSelection == "Rock") {
+                alert("comeback another time newbie, maybe when you'are strong enough")
+                computer++
+            } else if(playerSelection == "Rock" && computerSelection == "Paper") {
+                alert("You underestimated me, I'm a lot more skilled than I seem")
+                computer++
+            } else if(playerSelection == "Paper" && computerSelection == "Scissors") {
+                alert("YOU CAN NEVER DESTROY ME I'M INSTOPABLE!!! HAHAHHAHAHAH")
+                computer++
+            }
+    para.innerHTML = `Result: ${player} - ${computer}`;
+    
+    if (player === 5 && computer < 5 ) {
+        player = 0;
+        computer = 0;
+        alert("I won't GIVE Up easily EVEN IF I lost .ANotheR RouND")
+    }
+    if (computer === 5 && player < 5) {
+        player = 0;
+        computer = 0;
+        alert("noob such a pathetic weak human ROBOTS WILL TAKE OVER THE WORLD")
+    }
+ 
 }
-  //  playRound(playerSelection, computerSelection);
